0b776fb3b65ad609a410bd6dcf45d673d602af3c
Lani Villamor <lanivillamor@lanis-air.home>
2025-02-01 14:27:51 -0500

    update create user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,13 @@
 // const router = require("express").Router();
 const User = require("../models/user");
+
+const bcrypt = require("bcryptjs");
+
 const {
   BAD_REQUEST_400,
   NOT_FOUND_404,
   SERVER_ERROR_500,
+  DUPLICATE_ERROR_409,
 } = require("../utils/statusCodes");
 
 const getUsers = (req, res) => {
@@ -37,10 +41,32 @@ const getUserId = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-  User.create({ name, avatar })
-    .then((user) => res.send(user))
+  const { name, avatar, email, password } = req.body;
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      User.create({
+        name,
+        avatar,
+        email,
+        password: hash, // adding the hash to the database
+      });
+    })
+
+    .then((user) =>
+      res.status(201).send({
+        name: user.name,
+        avatar: user.avatar,
+        email: user.email,
+        _id: user._id,
+      })
+    ) //EXCLUDE SENDING PASSWORD DETAILS!
     .catch((error) => {
+      if (error.code === 11000) {
+        res
+          .status(DUPLICATE_ERROR_409)
+          .send({ message: "Error: User already exists" });
+      }
       if (error.name === "ValidationError") {
         return res
           .status(BAD_REQUEST_400)
