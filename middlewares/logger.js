3c7fc889ceaaaa88f0e0100f81807adb93e308da
Lani Villamor <lanivillamor@Lanis-MacBook-Air.local>
2025-04-03 14:51:08 -0400

    Fix logger express syntax errors and duplications

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -48,16 +48,4 @@ const requestLogger = expressWinston.logger({
 //   format: winston.format.json(),
 // });
 
-const errorLogger = expressWinston.errorLogger({
-  transports: [
-    new winston.transports.Console({
-      format: messageFormat,
-    }),
-    new winston.transports.File({
-      filename: "error.log",
-      format: winston.format.json(),
-    }),
-  ],
-});
-
-module.exports {requestLogger, errorLogger};
+module.exports = { requestLogger, errorLogger };
